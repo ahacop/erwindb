fa8e2506e9b30d7f37d36f33cdc208ceb27716a5
Ara Hacopian <ara@hacopian.de>
2026-02-01 01:03:34 +0100

    Bump version to 0.9.0

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "erwindb";
-            version = "0.1.0";
+            version = "0.9.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
 
